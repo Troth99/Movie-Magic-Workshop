a76f2c8a6176d2ad707a299a7355c72bccfc8b38
Troth99 <trothlol123456@gmail.com>
2025-09-28 06:21:24 +0300

    Added cast model

---
+++ src/models/Cast.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const castSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Cast name is required!']
+    },
+    age: {
+        type: Number,
+        required: [true, 'Cast age is required!']
+    },
+    born: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
+})
+
+const Cast = model('Cast', castSchema);
+
+export default Cast
