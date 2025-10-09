291ec5bdd34cf6070c9bed5e563f72ab7f8e210c
Troth99 <trothlol123456@gmail.com>
2025-10-09 04:49:34 +0300

    Cast validation

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -3,19 +3,26 @@ import { Schema, Types, model } from "mongoose";
 const castSchema = new Schema({
     name: {
         type: String,
-        required: [true, 'Cast name is required!']
+        required: [true, 'Cast name is required!'],
+        minLenght: [5, 'Name is to short!'],
+        match: [/^[a-zA-Z0-9 ]$/, 'Name has some invalid characters!'],
     },
     age: {
         type: Number,
-        required: [true, 'Cast age is required!']
+        required: [true, 'Cast age is required!'],
+        max: [120, 'Age cannot be more than 120!'],
+        min: [0, 'Age cannot be lower than 1!']
     },
     born: {
         type: String,
-        required: true
+        required: true,
+        minLenght: [10, 'Born should be atleast 10 characters long!'],
+        match: [/^[a-zA-Z0-9 ]$/, 'Born has some invalid characters!'],
     },
     imageUrl: {
         type: String,
-        required: true
+        required: true,
+        match: [/https?:\/\//, 'Image url is invalid!']
     }
 })
 
